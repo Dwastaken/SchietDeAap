56be30784321f3fdf1db176c4fc4049f5e97c49a
Dwastaken <d.winkelhorst456@gmail.com>
2025-06-04 09:13:38 +0200

    Character Controller

---
+++ schietaap/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float MouseSensitivity = 100f;
+    public Transform PlayerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Camera kijkt omhoog/omlaag
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Speler draait links/rechts
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}

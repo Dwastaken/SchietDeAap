c1c10ac3178c452c2bbd5ccf2b5c08e6b0bd7dcf
Dwastaken <d.winkelhorst456@gmail.com>
2025-06-04 14:01:11 +0200

    weaponswitching

---
+++ schietaap/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    void Start()
+    {
+        selectWeapon();
+    }
+
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        // Scroll omhoog
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else
+                selectedWeapon++;
+        }
+
+        // Scroll omlaag
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+                selectedWeapon = transform.childCount - 1;
+            else
+                selectedWeapon--;
+        }
+
+        // Toetsen 1 t/m 4
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+
+        // Alleen als het wapen is veranderd, roep je selectWeapon aan
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            selectWeapon();
+        }
+    }
+
+    void selectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            weapon.gameObject.SetActive(i == selectedWeapon);
+            i++;
+        }
+    }
+}

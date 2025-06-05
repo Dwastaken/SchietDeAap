1db3a9e0691f3dda32de4a3c2cdf803607aefbb5
Dwastaken <d.winkelhorst456@gmail.com>
2025-06-05 16:27:59 +0200

    grenadethrowbar

--- schietaap/Assets/Scripts/GrenadeThrow.cs
+++ schietaap/Assets/Scripts/GrenadeThrow.cs
@@ -1,24 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrenadeThrow : MonoBehaviour
 {
-    public float throwForce = 40f;
+    public float maxThrowForce = 40f;
+    public float chargeRate = 3f;
     public GameObject grenadePrefab;
-   
+    public Slider chargeSlider;
 
-    
+
+    private float currentThrowForce;
+    private bool isCharging;
+
+    private void Start()
+    {
+        chargeSlider.gameObject.SetActive(false);
+        chargeSlider.maxValue = maxThrowForce;
+    }
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.C))
 
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            ThrowGrenade();
+            isCharging = true;
+            currentThrowForce = 0f;
+            chargeSlider.gameObject.SetActive(true);
+        }
+
+
+        if (isCharging && Input.GetKey(KeyCode.C))
+        {
+            currentThrowForce += chargeRate * Time.deltaTime;
+            currentThrowForce = Mathf.Clamp(currentThrowForce, 0f, maxThrowForce);
+            chargeSlider.value = currentThrowForce;
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            isCharging = false;
+            ThrowGrenade(currentThrowForce);
+            chargeSlider.gameObject.SetActive(false);
         }
     }
-    void ThrowGrenade()
+
+    void ThrowGrenade(float force)
     {
-      GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
         Rigidbody rb = grenade.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        rb.AddForce(transform.forward * force, ForceMode.VelocityChange);
     }
 }
